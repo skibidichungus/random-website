8a1562c09f7e280431806db6a0929a5f74ae5401
Jaroslav <jaroslavt101@yahoo.com>
2026-03-11 01:58:35 -0500

    added darkmode and button toggle features

--- js/script.js
+++ js/script.js
@@ -1,3 +1,17 @@
-const button = document.getElementById("cta-button");
+const button = document.getElementById("toggle-btn");
+const extraText = document.getElementById("extra-text");
+const darkModeButton = document.getElementById("dark-mode-btn");
 
-button.addEventListener("click", function () { alert("Button clicked.."); });
+button.addEventListener("click", function () {
+  if (extraText.style.display === "block") {
+    extraText.style.display = "none";
+    button.textContent = "Show More";
+  } else {
+    extraText.style.display = "block";
+    button.textContent = "Show Less";
+  }
+});
+
+darkModeButton.addEventListener("click", function () {
+    document.body.classList.toggle("dark-mode");
+  });
